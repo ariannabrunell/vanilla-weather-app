5b01b72ba0c7efbaa9e905c35dc088f31c70a69a
ariannabrunell <83625264+ariannabrunell@users.noreply.github.com>
2021-06-17 22:41:59 -0500

    Added temperature high/low elements

--- src/app.js
+++ src/app.js
@@ -17,12 +17,16 @@ function formatDate(timestamp) {
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
+  let highElement = document.querySelector("#high");
+  let lowElement = document.querySelector("#low");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  highElement.innerHTML = Math.round(response.data.main.temp_max);
+  lowElement.innerHTML = Math.round(response.data.main.temp_min);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
