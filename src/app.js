a3542f631293d5de56e484f76086802edd5ba289
ariannabrunell <83625264+ariannabrunell@users.noreply.github.com>
2021-06-18 09:33:24 -0500

    Implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -30,7 +30,12 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
-  let iconeElement = document.querySelector("#icon");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+  celsiusTemperatureHigh = response.data.main.temp_max;
+  celsiusTemperatureLow = response.data.main.temp_min;
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   highElement.innerHTML = Math.round(response.data.main.temp_max);
   lowElement.innerHTML = Math.round(response.data.main.temp_min);
@@ -39,7 +44,7 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconeElement.setAttribute(
+  iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
@@ -57,5 +62,40 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = Math.round((celsiusTemperature * 9) / 5 + 32);
+  let temperatureElement = document.querySelector("#temperature");
+  let highTemperature = Math.round((celsiusTemperatureHigh * 9) / 5 + 32);
+  let lowTemperature = Math.round((celsiusTemperatureLow * 9) / 5 + 32);
+  let highElement = document.querySelector("#high");
+  let lowElement = document.querySelector("#low");
+  temperatureElement.innerHTML = fahrenheitTemperature;
+  highElement.innerHTML = highTemperature;
+  lowElement.innerHTML = lowTemperature;
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  let highElement = document.querySelector("#high");
+  highElement.innerHTML = Math.round(celsiusTemperatureHigh);
+  let lowElement = document.querySelector("#low");
+  lowElement.innerHTML = Math.round(celsiusTemperatureLow);
+}
+
+let celsiusTemperature = null;
+let celsiusTemperatureHigh = null;
+let celsiusTemperatureLow = null;
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Auckland");
